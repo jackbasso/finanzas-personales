3306728a7c99be75fd01ef840cf6040acef207e5
jackbasso <jackbasso.r@gmail.com>
2024-10-11 21:26:49 -0600

    v1.5

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -1,4 +1,3 @@
-// Adapted from https://github.com/shadcn-ui/ui/blob/main/apps/www/registry/default/ui/use-toast.ts
 import * as React from "react";
 
 import type { ToastActionElement, ToastProps } from "@/components/ui/toast";
@@ -86,7 +85,8 @@ export const reducer = (state: State, action: Action): State => {
     case "DISMISS_TOAST": {
       const { toastId } = action;
 
-      // ! Side effects ! //
+      // ! Side effects ! - This could be extracted into a dismissToast() action,
+      // but I'll keep it here for simplicity
       if (toastId) {
         addToRemoveQueue(toastId);
       } else {
